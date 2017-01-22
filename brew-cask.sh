3ca15c07cb6f3bfb4a8c5afb5f77ab76cbf29ee0
Jens Marquardt <dev@jensmarquardt.com>
2017-01-22 19:18:06 +0100

    Updated sorting

--- brew-cask.sh
+++ brew-cask.sh
@@ -3,19 +3,19 @@
 # Install Cask Room https://caskroom.github.io/
 brew tap caskroom/cask
 
+brew cask install adobe-creative-cloud
 brew cask install appcleaner
+brew cask install atom
 brew cask install dropbox
 brew cask install firefox
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install phpstorm
 brew cask install sequel-pro
-brew cask install sublime-text
-brew cask install spotify
 brew cask install skitch
 brew cask install slack
+brew cask install spotify
+brew cask install sublime-text
 brew cask install the-unarchiver
-brew cask install transmit
-brew cask install atom
 brew cask install tower
-brew cask install adobe-creative-cloud
+brew cask install transmit
